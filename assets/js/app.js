548fcc1f2dbdc55a40e490ccff12cbde72bddf51
Richard Schumann <schumanncombo@gmail.com>
2021-10-08 23:09:02 +0200

    add filters to steward cards
    - refactor the apps code
    - add filters to order stewards
    - adding some visual indicator for the current filter

--- assets/js/app.js
+++ assets/js/app.js
@@ -23,22 +23,144 @@ window.addEventListener('load', (event) => {
 
 
 
+
 function init(){
 
-	let imgpath = "assets/stewards/";
-	let gitcoinurl = "https://gitcoin.co/";
+	console.log("init...");
+
+	// map search input field to filterCard function
+	const search = document.getElementById("search")
+	search.addEventListener("input", () => {
+		filterStewards();
+	});
+
+	// map orderby input field to orderStewards function 
+	const orderby = document.getElementById("orderby")
+	orderby.addEventListener("input", () => {
+		orderStewards();
+	});
+
+	// map direction input field to orderStewards function 
+	const direction = document.getElementById("direction")
+	direction.addEventListener("input", () => {
+		orderStewards();
+	});
+
+	// reorder the stewards array on default order
+	orderStewards();
+	// add all the stewards to the #grid
+	draw();
+
+	// inspect location hash ( URL#search=xxx ) and get the params
+	params = getParams();
+	if (params){
+		// set search input field to params.search
+		search.value = params.search.toLowerCase();
+		// filter from all steward cards 
+		filterStewards();
+	}
+
+}
+
+
+
+
+
+
+// get params from location hash
+
+function getParams(){
+
+	var hash = window.location.hash.substr(1);
+
+	var params = hash.split('&').reduce(function (res, item) {
+	    var parts = item.split('=');
+	    res[parts[0]] = decodeURIComponent(parts[1]);
+	    return res;
+	}, {});
+
+	return params;
+}
+
+
+
+
+
+function filterStewards(){
+
+	search = document.getElementById("search") 
 	
-	console.log("init");
+	let datatags = document.querySelectorAll("[data-tags]")
+	//console.log(datatags)
 
-	let grid = document.querySelector('#grid');
-	let card = document.querySelector('#card');
+	searchInput = search.value.toLowerCase()
+	datatags.forEach(item => {
+		tags = item.dataset.tags.toLowerCase()
+		if ( tags.indexOf(searchInput) !== -1 ) { item.style.display = '' }
+		else { item.style.display = 'none' }		
+	});
 
+	document.location.hash = "search=" + encodeURIComponent(searchInput);
+
+}
+
+
+
+
+
+function orderStewards() {
+
+	orderby = document.getElementById("orderby").value
+	direction = document.getElementById("direction").value
+	// console.log(orderby, direction)
+
+	if (orderby == "health"){
+		window.stewards.sort((a, b) => (a.health < b.health) ? 1 : -1)
+	}
+
+	if (orderby == "weight"){
+		window.stewards.sort((a, b) => (a.votingweight < b.votingweight) ? 1 : -1)
+	}
+
+	if (orderby == "weight"){
+		window.stewards.sort((a, b) => (a.participation_snapshot < b.participation_snapshot) ? 1 : -1)
+	}	
+
+	if (orderby == "posts"){
+		window.stewards.sort((a, b) => (a.posts < b.posts) ? 1 : -1)
+	}	
+
+	// ascending - from low to high
+	if (direction == "ascending") {
+		window.stewards.reverse();
+	}
+
+	draw();
+
+}
+
+
+
+
+function draw(){
+
+	// console.log(window.stewards)
+
+	imgpath = "assets/stewards/";
+	gitcoinurl = "https://gitcoin.co/";
+
+	grid = document.querySelector('#grid');
+
+	// delete all inner nodes
+	grid.innerHTML = "";
+
+	card = document.querySelector('#card');
 
 	// generate all steward cards
 
 	window.stewards.forEach(steward => {
 
-		let clone = document.importNode(card.content, true);
+		clone = document.importNode(card.content, true);
 
 		tally_url = "https://www.withtally.com/voter/" + steward.address + "/governance/gitcoin";
 		statement_url = "https://gov.gitcoin.co/t/introducing-stewards-governance/41/" + steward.statement_post_id;
@@ -80,7 +202,32 @@ function init(){
 		}
 
 		// search tags
-		clone.querySelector('#card').dataset.tags = steward.name + steward.handle_gitcoin + steward.handle_forum + workstream_tag;
+		clone.querySelector('#card').dataset.tags = 
+		steward.address + " " + 
+		steward.name + " " + 
+		steward.handle_gitcoin + " " +
+		steward.handle_forum + " " +
+		workstream_tag;
+
+
+		// apply highlights to cards based on orderby
+		orderby = document.getElementById("orderby").value
+
+		if (orderby == "health"){
+			clone.querySelector('#health').classList.add('highlight');
+		}
+
+		if (orderby == "weight"){
+			clone.querySelector('#votingweight').classList.add('highlight');
+		}
+
+		if (orderby == "posts"){
+			clone.querySelector('#forum_post').classList.add('highlight');
+		}	
+
+		if (orderby == "participation"){
+			clone.querySelector('#participation_snapshot').classList.add('highlight');
+		}	
 
 		document.querySelector('#grid').appendChild(clone);
 
@@ -88,49 +235,10 @@ function init(){
 
 
 
-
-
-	// input field search function
-
-	const search = document.getElementById("search") 
-	const datatags = document.querySelectorAll("[data-tags]")
-
-	search.addEventListener("input", () => {
-		searchInput = search.value.toLowerCase()
-		datatags.forEach(item => {
-			tags = item.dataset.tags.toLowerCase()
-			if ( tags.indexOf(searchInput) !== -1 ) { item.style.display = '' }
-			else { item.style.display = 'none' }		
-		});
-
-		document.location.hash = "search=" + encodeURIComponent(searchInput);
-
-	});
-
-
-	// build params from locatio.hash
-
-	var hash = window.location.hash.substr(1);
-
-	var params = hash.split('&').reduce(function (res, item) {
-	    var parts = item.split('=');
-	    res[parts[0]] = decodeURIComponent(parts[1]);
-	    return res;
-	}, {});
-
-	// if params.search filter view & set input field
-
-	if (params.search){
-		search.value = params.search.toLowerCase()
-		searchInput = params.search.toLowerCase()
-		datatags.forEach(item => {
-			tags = item.dataset.tags.toLowerCase()
-			if ( tags.indexOf(searchInput) !== -1 ) { item.style.display = '' }
-			else { item.style.display = 'none' }		
-		});
-
-	}
-
-
 }
+
+
+
+
+
 
