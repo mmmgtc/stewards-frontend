b402825d7448ecd5eee4e2ad423602e766afe890
Nazeeh Vahora <nazeehvahora.786@gmail.com>
2022-07-06 12:44:54 +0530

    Add input boxes

--- components/InputLayout/index.tsx
+++ components/InputLayout/index.tsx
@@ -1,23 +1,25 @@
-import { Box, Text } from "@chakra-ui/react";
+import { Stack, Text } from "@chakra-ui/react";
 
 const InputLayout: React.FC<{ label: string; children: JSX.Element }> = ({
   label,
   children,
 }) => {
   return (
-    <Box
+    <Stack
       textAlign="left"
       fontSize="1.1rem"
       background="#321e5e"
       borderRadius="6px"
       p="1rem"
       w={"full"}
+      height='7rem'
+      justifyItems='center'
     >
       <Text mb="0.5rem" color="#be59cf">
         {label}
       </Text>
       {children}
-    </Box>
+    </Stack>
   );
 };
 
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,9 +1,18 @@
-import { Box, Button, Flex, Grid, GridItem, Heading, HStack, Input, Link, Text } from "@chakra-ui/react";
+import {
+  Flex,
+  Grid,
+  GridItem,
+  Heading,
+  Input,
+  Link,
+  Text,
+} from "@chakra-ui/react";
 import type { NextPage } from "next";
-
+import { useState } from "react";
 import { useAccount, useConnect, useDisconnect } from "wagmi";
 import { InjectedConnector } from "wagmi/connectors/injected";
 import InputLayout from "../components/InputLayout";
+import SelectInput from "../components/SelectInput";
 
 const Home: NextPage = () => {
   const { address, isConnected } = useAccount();
@@ -11,6 +20,10 @@ const Home: NextPage = () => {
     connector: new InjectedConnector(),
   });
   const { disconnect } = useDisconnect();
+
+  const [orderBy, setOrderBy] = useState("");
+  const [display, setDisplay] = useState("ascending");
+  const [time, setTime] = useState("");
 
   return (
     <Flex
@@ -23,7 +36,7 @@ const Home: NextPage = () => {
       <Heading mb="2rem" textAlign="center">
         Steward Health Cards
       </Heading>
-      <Text mb='2rem'>
+      <Text mb="2rem">
         The Stewards of Gitcoin DAO play a vital role in driving the Gitcoin
         ecosystem forward through their work in governance and workstreams. In
         an effort to boost transparency the MMM-Workstream have created this
@@ -36,16 +49,51 @@ const Home: NextPage = () => {
         , to learn more and get involved - visit{" "}
         <Link href="https://gitcoindao.com/">GitcoinDAO.com</Link>
       </Text>
-      <Text mb='2rem'>
+      <Text mb="2rem">
         Data powered by <Link href="https://www.showkarma.xyz/">Karma</Link>.
       </Text>
 
-      <Grid w='full' templateColumns='repeat(5, 1fr)' gap={6}>
-          <GridItem colSpan={2}>
-            <InputLayout label="Search">
-              <Input border='none' p={0} focusBorderColor="none" color='white' fontSize='1.3rem' placeholder="Name, Address, WorkStream ..." />
-            </InputLayout>
-          </GridItem>
+      <Grid w="full" templateColumns="repeat(5, 1fr)" gap={6}>
+        <GridItem colSpan={2}>
+          <InputLayout label="Search">
+            <Input
+              border="none"
+              p={0}
+              focusBorderColor="none"
+              color="white"
+              fontSize="1.3rem"
+              placeholder="Name, Address, WorkStream ..."
+            />
+          </InputLayout>
+        </GridItem>
+        <GridItem>
+          <SelectInput
+            label="Order by"
+            options={[
+              { label: "health", value: "health" },
+              { label: "Forum Activity", value: "forum_activity" },
+              { label: "Voting Weight", value: "voting_weight" },
+            ]}
+            onChange={setDisplay}
+          />
+        </GridItem>
+        <GridItem>
+          <SelectInput
+            label="Display"
+            options={[
+              { label: "Descending", value: "descending" },
+              { label: "Ascending", value: "ascending" },
+            ]}
+            onChange={setOrderBy}
+          />
+        </GridItem>
+        <GridItem>
+          <SelectInput
+            label="Time"
+            options={[{ label: "Lifetime", value: "lifetime" }]}
+            onChange={setTime}
+          />
+        </GridItem>
       </Grid>
     </Flex>
   );
