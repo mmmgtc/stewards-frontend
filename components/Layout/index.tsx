e92d224d40a7d9c1df0abc15f2ff0dc82ba3c313
Gerrit Giliomee <giliomeejg@gmail.com>
2022-07-11 16:31:21 +0200

    func(Filtering & sorting): Add filtering and sorting mechanism

--- components/Layout/index.tsx
+++ components/Layout/index.tsx
@@ -1,11 +1,12 @@
 import { VStack } from "@chakra-ui/react";
+import { BrowserRouter as Router } from 'react-router-dom';
 
 interface LayoutProps {
   children: JSX.Element;
 }
 
-const Layout: React.FC<LayoutProps> = ({children}) => {
-  return <VStack w='90vw' m={'auto'} py='4rem'>{children}</VStack>
+const Layout: React.FC<LayoutProps> = ({ children }) => {
+  return <Router><VStack w='90vw' m={'auto'} py='4rem'>{children}</VStack></Router>
 }
 
 export default Layout;
--- pages/index.tsx
+++ pages/index.tsx
@@ -9,6 +9,8 @@ import {
 } from "@chakra-ui/react";
 import type { NextPage } from "next";
 import { useState, useEffect } from "react";
+import { useSearchParams } from 'react-router-dom';
+
 // import { useAccount, useConnect, useDisconnect } from "wagmi";
 // import { InjectedConnector } from "wagmi/connectors/injected";
 import Footer from "../components/Footer";
@@ -23,53 +25,100 @@ const Home: NextPage = () => {
   // });
   // const { disconnect } = useDisconnect();
 
-  const [orderBy, setOrderBy] = useState("");
-  const [display, setDisplay] = useState("ascending");
-  const [time, setTime] = useState("");
+  const [searchParams, setSearchParams] = useSearchParams();
+  const [search, setSearch] = useState(searchParams.get("search") ?? '');
+  const [orderBy, setOrderBy] = useState(searchParams.get("orderBy") ?? 'forum_activity');
+  const [display, setDisplay] = useState(searchParams.get("display") ?? 'descending');
+  const [time, setTime] = useState(searchParams.get("time") ?? '30d');
+  const [stewardsData, setStewardsData] = useState([]);
+  const [filteredStewardsData, setFilteredStewardsData] = useState([]);
 
   /**
    * Find a steward profile by the passed in wallet address and data to search through
    */
-function findStewardByAddress(address, data) {
-  return data.find(e => {
-    if (e.address === address) {
-//      console.log('found', e);
-    }
-    return e.address === address});
-}
-
-/**
- * Combine steward data from stewards_data.json, which is at the time of writing manually updated, and karma data, which is coming from the karma API.
- */
-async function getStewardsData() {
-  const success = res => res.ok ? res.json() : Promise.resolve({});
-
-  const karmaData = fetch("/api.showkarma.xyz/api/dao/delegates?name=gitcoin&pageSize=250&offset=0&workstreamId=6,4,3,7,1,2,5&period=30d").then(success);
-  const stewardsProfileData = fetch("/assets/stewards/stewards_data.json").then(success);
-
-  let ret = [];
-
-  await Promise.all([karmaData, stewardsProfileData]).then(([karmaData, stewardsProfileData]) => {
-    karmaData.data.delegates.forEach(element => {
-      element.profile = findStewardByAddress(element.publicAddress, stewardsProfileData.data);
-      console.log(element);
-      ret.push(element);
+  function findStewardByAddress(address, data) {
+    return data.find(e => {
+      return e.address === address
     });
+  }
 
-}).catch(err => console.error(err));
+  /**
+   * Combine steward data from stewards_data.json, which is at the time of writing manually updated, and karma data, which is coming from the karma API.
+   */
+  async function getStewardsData() {
+    const success = res => res.ok ? res.json() : Promise.resolve({});
 
- return ret;
-}
+    const karmaData = fetch("/api.showkarma.xyz/api/dao/delegates?name=gitcoin&pageSize=250&offset=0&workstreamId=6,4,3,7,1,2,5&period=" + time).then(success);
+    const stewardsProfileData = fetch("/assets/stewards/stewards_data.json").then(success);
 
-const [stewardsData, setStewardsData] = useState([]);
+    let ret = [];
 
-// Get data on load
-useEffect(() => {
-  getStewardsData().then(data => {
-  setStewardsData(data);
-});
-},[]);
+    await Promise.all([karmaData, stewardsProfileData]).then(([karmaData, stewardsProfileData]) => {
+      karmaData.data.delegates.forEach(element => {
+        element.profile = findStewardByAddress(element.publicAddress, stewardsProfileData.data);
+        ret.push(element);
+      });
 
+    }).catch(err => console.error(err));
+
+    return ret;
+  }
+
+  /**
+   * Apply some filtering to the stewards data
+   */
+  function filterStewardsData(data) {
+    let clonedData = JSON.parse(JSON.stringify(data));
+
+    if (search.length > 0) {
+      clonedData = clonedData.filter(element => {
+        if (element.profile) {
+          return element.profile.name.toLowerCase().indexOf(search.toLowerCase()) >= 0;
+        }
+      });
+    }
+
+    // TODO:::The fields on which sorting happens needs to be checked, e.g. health
+    if (orderBy === 'health') {
+      clonedData.sort(function (a, b) {
+        return a.stats[0].karmaScore - b.stats[0].karmaScore;
+      });
+    } else if (orderBy === 'forum_activity') {
+      console.log('forum_activity');
+      clonedData.sort(function (a, b) {
+        return a.stats[0].forumActivityScore - b.stats[0].forumActivityScore;
+      });
+    } else if (orderBy === 'voting_weight') {
+      clonedData.sort(function (a, b) {
+        return parseInt(a.stats[0].onChainVotesPct) - parseInt(b.stats[0].onChainVotesPct);
+      });
+    }
+
+    if (display === 'descending') {
+      clonedData = clonedData.reverse();
+    }
+
+    console.log('clonedData: ', clonedData);
+
+
+    setFilteredStewardsData(clonedData);
+  }
+
+  // Get data on load, as well as if the time changes
+  useEffect(() => {
+    getStewardsData().then(data => {
+      setStewardsData(data);
+      filterStewardsData(data);
+    });
+  }, [time]);
+
+
+  // Set the query params
+  useEffect(() => {
+    setSearchParams({ 'search': search, 'orderBy': orderBy, 'display': display, 'time': time });
+
+    filterStewardsData(stewardsData);
+  }, [search, orderBy, display, time]);
 
 
   return (
@@ -109,6 +158,8 @@ useEffect(() => {
         <GridItem colSpan={{ lg: 2, base: 1 }}>
           <InputLayout label="Search">
             <Input
+              onChange={(e) => setSearch(e.target.value)}
+              value={search}
               border="none"
               p={0}
               focusBorderColor="none"
@@ -126,6 +177,7 @@ useEffect(() => {
               { label: "Forum Activity", value: "forum_activity" },
               { label: "Voting Weight", value: "voting_weight" },
             ]}
+            defaultValue={orderBy}
             onChange={setDisplay}
           />
         </GridItem>
@@ -136,13 +188,15 @@ useEffect(() => {
               { label: "Descending", value: "descending" },
               { label: "Ascending", value: "ascending" },
             ]}
-            onChange={setOrderBy}
+            defaultValue={display}
+            onChange={setDisplay}
           />
         </GridItem>
         <GridItem>
           <SelectInput
             label="Time"
-            options={[{ label: "Lifetime", value: "lifetime" }]}
+            options={[{ label: "30 Days", value: "30d" }, { label: "Lifetime", value: "lifetime" }]}
+            defaultValue={time}
             onChange={setTime}
           />
         </GridItem>
@@ -156,17 +210,21 @@ useEffect(() => {
         }}
         gap={"2rem"}
       >
-        {stewardsData.map((element, index) => (
+        {filteredStewardsData.map((element, index) => (
           <GridItem key={index}>
             <StewardsCard
-              name={element.profile ? element.profile.name : 'Unknown'}
-              gitcoinUsername={element.profile ? element.profile.gitcoin_username : 'Unknown'}
+              name={element.profile ? element.profile.name : '-'}
+              gitcoinUsername={element.profile ? element.profile.gitcoin_username : '-'}
               profileImage={element.profile ? element.profile.profile_image : ''}
-              stewardsSince={element.profile ? element.profile.steward_since : ''}
-              activity={6}
-              workstream="MMM Lead"
-              voting={0.01}
-              participation={67}
+              stewardsSince={element.profile ? element.profile.steward_since : '-'}
+              activity={element.stats[0].forumActivityScore}
+              workstream={element.workstreams.map(item => { return item.name }).join(',')}
+              voting={element.stats[0].delegatedVotes / 1000000}
+              participation="0"
+              statementLink={element.profile ? element.profile.statement_post : ''}
+              delegateLink={'https://www.withtally.com/voter/' + element.address + '/governance/gitcoin'}
+              forumActivityLink={element.profile ? 'https://gov.gitcoin.co/u/' + element.profile.gitcoin_username : '/'}
+              healthScore={element.stats[0].gitcoinHealthScore}
             />
           </GridItem>
         ))}
