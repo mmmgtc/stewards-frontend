0addc7ab0962c896f84d2f5329fb8f328ccc2bd6
Gerrit Giliomee <giliomeejg@gmail.com>
2022-07-14 12:35:31 +0200

    chore(Footer): Set image width & height

--- components/Footer/index.tsx
+++ components/Footer/index.tsx
@@ -8,12 +8,12 @@ const Footer = () => {
     <VStack gap={10} mt={"20"}>
       <footer>
         <Link href="https://m-m-m.xyz/" target="_blank">
-          <img src="assets/mmm.svg" alt="mmm" />
+          <img src="assets/mmm.svg" alt="mmm" width="96" height="32" />
         </Link>
       </footer>
       <section id="gitcoindao">
         <Link href="https://gitcoindao.com" target="_blank">
-          <img src="assets/gitcoindao.svg" alt="gitcoindao" />
+          <img src="assets/gitcoindao.svg" alt="gitcoindao" width="188" height="58" />
         </Link>
       </section>
     </VStack>
